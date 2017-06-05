3be2f8040a63144ba592ba971d50d68f7ed9f48a
Ben Sherman <bcsherma@ucsc.edu>
2017-06-05 10:31:11 -0700

    just needed to push everything

--- simple/gen.py
+++ simple/gen.py
@@ -1,9 +1,10 @@
 import random
 
 seen = {}
-for i in range(20):
-    x = random.randint(0,1000)
+i = 0
+while i < 5:
+    x = random.randint(10,100)
     if x not in seen:
         print "item(%d)." % (x)
         seen[x] = 1
-    
+        i+=1
--- simple/process.py
+++ simple/process.py
@@ -10,6 +10,7 @@ atoms = result['Call'][0]['Witnesses'][0]['Value']
 items = []
 modulus = 0
 coef = 0
+exponent = 0
 
 for a in atoms:
     match = re.match('item\(([0-9]*)\)', a)
@@ -19,16 +20,20 @@ for a in atoms:
     match = re.match('mod\(([0-9]*)\)', a)
     if match:
         modulus = int(match.group(1))
+
+    match = re.match('exp\(([0-9]*)\)', a)
+    if match:
+        exponent = int(match.group(1))
         
     match = re.match('coef\(([0-9]*)\)', a)
     if match:
         coef = int(match.group(1))
 
         
-table = [(item*coef%modulus, item) for item in items]
+table = [(item*coef**exponent%modulus, item) for item in items]
 table.sort()
 
-print "Hash function: %dx mod %d" %(coef,modulus)
+print "Hash function: %dx^%d mod %d" %(coef,exponent,modulus)
 
 print "%7s %7s" % ("hash", "item")
 for item in table:
