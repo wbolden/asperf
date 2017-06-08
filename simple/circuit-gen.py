bd950d0ce63d3455fc8cf60a13abde961aa22328
Will Bolden <wbolden@ucsc.edu>
2017-06-07 20:46:11 -0700

    Some refactoring, circuit modifications, opt modifications

--- simple/circuit-gen.py
+++ simple/circuit-gen.py
@@ -32,12 +32,37 @@ endmodule
 """
 verilog_src = \
 """
+module shr4(shr4in, shr4out);
+    parameter WIDTH = {0};
+    input [WIDTH-1:0] shr4in;
+    output [WIDTH-1:0] shr4out;
+    
+    assign shr4out = shr4in >> 4;
+endmodule
+
+
+module mod53(modin1,modout);
+    parameter WIDTH = {0};
+    input [WIDTH-1:0] modin1;
+    output [WIDTH-1:0] modout;
+    assign modout = modin1 % 53;
+endmodule
+
+
 module mod(modin1,modin2,modout);
     parameter WIDTH = {0};
     input [WIDTH-1:0] modin1;
     input [WIDTH-1:0] modin2;
     output [WIDTH-1:0] modout;
     assign modout = modin1 % modin2;
+endmodule
+
+module shr(shrin1,shrin2,shrout);
+    parameter WIDTH = {0};
+    input [WIDTH-1:0] shrin1;
+    input [WIDTH-1:0] shrin2;
+    output [WIDTH-1:0] shrout;
+    assign shrout = shrin1 >> shrin2;
 endmodule
 
 module mul(mulin1,mulin2,mulout);
@@ -69,11 +94,13 @@ outfile = open('tmp.v','w')
 outfile.write(verilog_src)
 outfile.close()
 
+
+
 FNULL = open(os.devnull, 'w')
 subprocess.call(yosys_cmd,shell=True,stdout=FNULL)
 
 modules = json.load(open('tmp.json'))['modules']
-outfile = open('multiplier.lp','w')
+outfile = open('circuit_lib.lp','w')
 for m in modules:
     outfile.write('device({}).\n'.format(m))
     dev = modules[m]
@@ -84,7 +111,11 @@ for m in modules:
         count += 1
         outfile.write('device_port_width({},{},{}).\n'.format(m,p,len(port['bits'])))
         for i,b in enumerate(port['bits']):
-            outfile.write('device_port_bit_wire({},{},{},{}).\n'.format(m,p,i,b))
+            
+            if(type(b) != int):
+                outfile.write('device_port_bit_literal({},{},{},{}).\n'.format(m,p,i,b))
+            else:
+                outfile.write('device_port_bit_wire({},{},{},{}).\n'.format(m,p,i,b))
     for c in dev['cells'].values():
         con = c['connections']
         if c['type'] == 'AND':
@@ -94,5 +125,5 @@ for m in modules:
         if c['type'] == 'NOT':
             outfile.write('device_gate({},not_gate,({}, {})).\n'.format(m,con['A'][0],con['Y'][0])) 
 outfile.close()
-os.remove('tmp.v')
-os.remove('tmp.json')
+#%os.remove('tmp.v')
+#os.remove('tmp.json')
